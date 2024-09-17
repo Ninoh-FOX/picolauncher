75ded8805ed94814dc595b1bcb36125848d08de4
Ninoh-FOX <ninohfox@gmail.com>
2024-09-17 14:23:25 +0200

    add cross build for miyoo mini

--- dev/scripts/build-linux-armv7.sh
+++ dev/scripts/build-linux-armv7.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# Export environment variables to use the correct toolchain
+export CC=/opt/miyoomini-toolchain/usr/bin/arm-linux-gnueabihf-gcc
+export CXX=/opt/miyoomini-toolchain/usr/bin/arm-linux-gnueabihf-g++
+export AR=/opt/miyoomini-toolchain/usr/bin/arm-linux-gnueabihf-ar
+export SYSROOT=/opt/miyoomini-toolchain/arm-buildroot-linux-gnueabihf/sysroot
+export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=$CC
+export CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_RUSTFLAGS="-C link-arg=--sysroot=$SYSROOT"
+
 build_dir=build/build-linux-armv7
 
 # create file structure
@@ -13,15 +21,14 @@ mkdir -p $build_dir/drive/screenshots/
 mkdir -p $build_dir/drive/carts/
 mkdir -p $build_dir/drive/carts/screenshots/
 
-# build executables
-cross build --release --target armv7-unknown-linux-gnueabihf
+# build executables using the standard Rust target
+cargo build --release
 
 # install files
-cp -r drive/carts/{games,labels,metadata,music} $build_dir/drive/carts
-cp -r drive/carts/{*.p8,*.lua} $build_dir/drive/carts/
+cp -r drive/carts/ $build_dir/drive/carts
 cp drive/config_template.txt $build_dir/drive/config.txt
 
 cp target/armv7-unknown-linux-gnueabihf/release/picolauncher $build_dir/picolauncher
 cp target/armv7-unknown-linux-gnueabihf/release/p8util $build_dir/p8util
 
-zip -r picolauncher-linux-armv7.zip $build_dir
+zip -r picolauncher-linux-miyoomini.zip $build_dir
